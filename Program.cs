82b01957e1303020ba904e33dc157751742c42e8
Yuri Liashenko <yuri.lyashenko@gmail.com>
2024-08-06 23:35:52 -0500

    anagram strings solution

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+namespace leetcode_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Anagram");
+       Console.WriteLine("Now enter S :");
+       String s = new String(Convert.ToString(Console.ReadLine()));
+       Console.WriteLine("Now enter S :");
+       String t = new String(Convert.ToString(Console.ReadLine()));
+       bool answer = false;
+        if (s.Length == t.Length){
+
+        }
+
+
+
+       Console.WriteLine(answer.ToString()+s.Length);
+
+    }
+}
+
